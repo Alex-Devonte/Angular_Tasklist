255b064b0385a4a9511c4f41eb1be16cae777356
Alex-Devonte <alexdevontetaylor@gmail.com>
2018-11-25 16:16:23 -0500

    Added functionality for editing and updating tasks

--- src/app/task-detail/task-detail.component.ts
+++ src/app/task-detail/task-detail.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Task } from "../task";
+import { TaskDataService } from '../task-data.service';
 
 @Component({
   selector: 'app-task-detail',
   templateUrl: './task-detail.component.html',
   styleUrls: ['./task-detail.component.css']
 })
-export class TaskDetailComponent implements OnInit {
 
-  constructor() { }
+export class TaskDetailComponent implements OnInit {
+  @Input() task: Task;
+  constructor(private route: ActivatedRoute, private taskDataService: TaskDataService, private location: Location) { }
 
   ngOnInit() {
+    this.getTask();
+  }
+
+  getTask(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.taskDataService.getTask(""+id).subscribe(task => this.task = task[0]);
+  }
+
+  //Return user to previous page
+  back(): void {
+    this.location.back();
+  }
+
+  //save updated task
+  save(): void {
+    this.taskDataService.updateTask(this.task)
+    .subscribe(() => this.back());
   }
 
 }
